e65d68cbae963b8a0c413e9e785ca405a7c6f760
Alex Andrews <alexcandrews91@gmail.com>
2016-12-20 14:20:05 -0700

    characters page without pagination

--- grow_backend_test/routes/index.js
+++ grow_backend_test/routes/index.js
@@ -36,17 +36,32 @@ router.get('/characters/:name', function(req, res, next) {
 
 router.get('/characters', function(req, res, next) {
 	
+	request('http://www.swapi.co/api/people', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			body = JSON.parse(body);
+			if (body.count == 0) {
+				res.render('error', {
+					message: "Error: invalid character",
+				});
+			} else {
+				// for (var i = 0; i < body.residents.length; i++) {
+					// console.log(i);
+				// }
+				res.send(body);
+				// character_data = body.results;
+    			// res.render('character', {
+	 				// title: 'Character - ' + character_data[0].name,
+	 				// data: JSON.stringify(character_data)
+				// });
+    		} 
+		}
+	});
 
 });
 
 router.get('/planetresidents', function(req, res, next) {
-	request({
-    	uri: 'http://www.swapi.co/api/planets',
-    	qs: {
-      	// api_key: '123456',
-      	// query: 'World of Warcraft: Legion'
-    	}
-  	}).pipe(res);
+
+	
 });
 
 module.exports = router;
