f9bf4db03cb0952e92a413803fb417435e1a0944
Alex Andrews <alexcandrews91@gmail.com>
2016-12-20 15:18:11 -0700

    implemented pagination, successfully get 50 characters

--- grow_backend_test/routes/index.js
+++ grow_backend_test/routes/index.js
@@ -11,11 +11,14 @@ router.get('/', function(req, res, next) {
 });
 
 
+// GET characters by name
 router.get('/characters/:name', function(req, res, next) {
 	
 	request('http://www.swapi.co/api/people/?search=' + req.params.name, function (error, response, body) {
+		// if call is successful
 		if (!error && response.statusCode == 200) {
 			body = JSON.parse(body);
+			// make sure the call returns data, error if it doesn't
 			if (body.count == 0) {
 				res.render('error', {
 					message: "Error: invalid character",
@@ -32,11 +35,46 @@ router.get('/characters/:name', function(req, res, next) {
 
 });
 
-
-
+// GET all characters (truncate to just 50)
 router.get('/characters', function(req, res, next) {
-	
-	request('http://www.swapi.co/api/people', function (error, response, body) {
+
+	// this is the array of our data we will add to
+	var character_data = [];
+	// use page_counter to paginate
+	var get_character_data = function(page_counter) {
+
+		request({
+			url: 'http://www.swapi.co/api/people/?page=' + page_counter
+		}, function (error, response, body) {
+
+			if(!error && response.statusCode === 200) {
+				body = JSON.parse(body);
+			
+				// push each element in 'results' into our character array
+				for (var i = 0; i < body.results.length; i++) {
+					character_data.push(body.results[i]);	
+				}
+				// exit once we reach 50 characters
+				if(character_data.length == 50) {
+
+	                res.send(character_data);
+	            } else {
+					get_character_data(page_counter + 1);
+				}
+
+			} else {
+				res.render('error', {
+					message: "Error: invalid character",
+				});
+			}
+		});
+	}
+	get_character_data(1);
+});
+
+router.get('/planetresidents', function(req, res, next) {
+
+	request('http://www.swapi.co/api/planets', function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			body = JSON.parse(body);
 			if (body.count == 0) {
@@ -56,12 +94,6 @@ router.get('/characters', function(req, res, next) {
     		} 
 		}
 	});
-
-});
-
-router.get('/planetresidents', function(req, res, next) {
-
-	
 });
 
 module.exports = router;
