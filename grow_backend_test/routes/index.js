7c02ff699566e748cce9f2d95a1c5af26b499190
Alex Andrews <alexcandrews91@gmail.com>
2016-12-20 14:12:03 -0700

    get named character, added error page for invalids

--- grow_backend_test/routes/index.js
+++ grow_backend_test/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var async = require('async');
+
+var character_data;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,26 +11,42 @@ router.get('/', function(req, res, next) {
 });
 
 
+router.get('/characters/:name', function(req, res, next) {
+	
+	request('http://www.swapi.co/api/people/?search=' + req.params.name, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			body = JSON.parse(body);
+			if (body.count == 0) {
+				res.render('error', {
+					message: "Error: invalid character",
+				});
+			} else {
+				character_data = body.results;
+    			res.render('character', {
+	 				title: 'Character - ' + character_data[0].name,
+	 				data: JSON.stringify(character_data)
+				});
+    		} 
+		}
+	});
+
+});
+
+
+
 router.get('/characters', function(req, res, next) {
-  var test_res = res;
-  for (var i = 0; i < 3; i++) {
-  	request({
-    	uri: 'http://www.swapi.co/api/people/'+i,
+	
+
+});
+
+router.get('/planetresidents', function(req, res, next) {
+	request({
+    	uri: 'http://www.swapi.co/api/planets',
     	qs: {
       	// api_key: '123456',
       	// query: 'World of Warcraft: Legion'
     	}
-  	}).pipe(test_res);
-  }
-  res = test_res;
-});
-
-
-// GET about page
-router.get('/about', function(req, res){
-  res.render('about', {
-    title: 'About'
-  });
+  	}).pipe(res);
 });
 
 module.exports = router;
