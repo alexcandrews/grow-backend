f9dd83acbb210991f944775c1c3d87f375aeeb6d
Alex Andrews <alexcandrews91@gmail.com>
2016-12-20 11:49:59 -0700

    test adding page; establish cconnect to swapi

--- grow_backend_test/routes/index.js
+++ grow_backend_test/routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.get('/characters', function(req, res, next) {
+  var test_res = res;
+  for (var i = 0; i < 3; i++) {
+  	request({
+    	uri: 'http://www.swapi.co/api/people/'+i,
+    	qs: {
+      	// api_key: '123456',
+      	// query: 'World of Warcraft: Legion'
+    	}
+  	}).pipe(test_res);
+  }
+  res = test_res;
+});
+
+
+// GET about page
+router.get('/about', function(req, res){
+  res.render('about', {
+    title: 'About'
+  });
 });
 
 module.exports = router;
