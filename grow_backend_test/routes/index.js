492eaa0b693c62e45ad4d695ecd839eaf867a79f
Alex Andrews <alexcandrews91@gmail.com>
2016-12-20 21:00:03 -0700

    implemented retrieving planets & residents

--- grow_backend_test/routes/index.js
+++ grow_backend_test/routes/index.js
@@ -92,7 +92,7 @@ router.get('/characters', function(req, res, next) {
 
 			} else {
 				res.render('error', {
-					message: "Error: invalid character",
+					message: "Error:",
 				});
 			}
 		});
@@ -100,28 +100,93 @@ router.get('/characters', function(req, res, next) {
 	get_character_data(1);
 });
 
+var create_planet_resident_obj = function(res, planet_data, resident_data) {
+
+	var planet_resident_data = {};
+
+	// for each planet in planet_data
+	for (var i = 0; i < planet_data.length; i++) {
+		var residents = [];
+		// for each resident in planet
+		for (var j = 0; j < planet_data[i]['residents'].length; j++) {
+			// get each residents url
+			var url = planet_data[i]['residents'][j];
+			// find the matching resident url
+			var resident_url = resident_data.filter(function( obj ) {
+				return obj['url'] == url;
+			});
+			residents.push(resident_url[0]['name']);
+		}
+		planet_resident_data[planet_data[i]['name']] = residents;
+	}
+	// display the data
+	res.send(planet_resident_data);
+}
+
+// GET all planets, and each resident for each planet
 router.get('/planetresidents', function(req, res, next) {
 
-	request('http://www.swapi.co/api/planets', function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			body = JSON.parse(body);
-			if (body.count == 0) {
+	// this is the array of our data we will add to
+	var planet_data = [];
+	var resident_data = [];
+
+	var get_resident_data = function(page_counter, planet_data) {
+		request({
+			url: 'http://www.swapi.co/api/people/?page=' + page_counter
+		}, function (error, response, body) {
+
+			if(!error && response.statusCode === 200) {
+				body = JSON.parse(body);
+			
+				// push each element in 'results' into our character array
+				for (var i = 0; i < body.results.length; i++) {
+					resident_data.push(body.results[i]);	
+				}
+				// exit once we reach 50 characters
+				if(resident_data.length == 87) {
+					create_planet_resident_obj(res, planet_data, resident_data);	                
+	                // res.send(planet_data);
+	            } else {
+					get_resident_data(page_counter + 1, planet_data);
+				}
+
+			} else {
+				res.render('error', {
+					message: "Error:",
+				});
+			}
+		});
+	}
+
+	// use page_counter to paginate
+	var get_planet_data = function(page_counter) {
+
+		request({
+			url: 'http://www.swapi.co/api/planets/?page=' + page_counter
+		}, function (error, response, body) {
+
+			if(!error && response.statusCode === 200) {
+				body = JSON.parse(body);
+
+				for (var i = 0; i < body.results.length; i++) {
+					planet_data.push(body.results[i]);
+				}
+
+				if(body.results.length < 10) {
+	                get_resident_data(1, planet_data);
+	                // res.send(planet_data);
+	            } else {
+					get_planet_data(page_counter + 1);
+				}
+			} else {
 				res.render('error', {
 					message: "Error: invalid character",
 				});
-			} else {
-				// for (var i = 0; i < body.residents.length; i++) {
-					// console.log(i);
-				// }
-				res.send(body);
-				// character_data = body.results;
-    			// res.render('character', {
-	 				// title: 'Character - ' + character_data[0].name,
-	 				// data: JSON.stringify(character_data)
-				// });
-    		} 
-		}
-	});
+			}
+		});
+	}
+	get_planet_data(1);	
+
 });
 
 module.exports = router;
